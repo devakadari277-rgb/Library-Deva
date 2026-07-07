d33bc5daed80ba55b837add0606dd78ef8ad99b7
Deva Kadari <devakadari277@gmail.com>
2026-07-07 15:45:51 +0530

    Update db uri logic for postgresql on vercel

--- config.py
+++ config.py
@@ -5,9 +5,12 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey123')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        f'sqlite:///{os.path.join(BASE_DIR, "database.db")}')
+    # Handle postgres:// vs postgresql:// for SQLAlchemy
+    db_url = os.environ.get('DATABASE_URL')
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+        
+    SQLALCHEMY_DATABASE_URI = db_url or f'sqlite:///{os.path.join(BASE_DIR, "database.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REMEMBER_COOKIE_DURATION = timedelta(days=7)
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'images')
